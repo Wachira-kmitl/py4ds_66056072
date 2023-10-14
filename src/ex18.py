629af2af619de1c65981cbdf48198d156abc8ab8
admin <66056072@kmitl.ac.th>
2023-10-14 21:33:06 +0700

    test_ex18

--- src/ex18.py
+++ src/ex18.py
@@ -2,6 +2,13 @@
 Exercise 18 : Buy 8 get 1 free
 """
 
+def get_cost_of_coffee(glass, price):
+    free = glass // 9
+    paid = glass - free
+    return paid * price
 
-def get_cost_of_coffee(param, param1):
-    pass
+
+def get_cost_of_coffee_2(x, price):
+    free = x // 9
+    paid = x - free
+    return paid * price
