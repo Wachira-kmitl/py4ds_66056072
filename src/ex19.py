d9949bc44e7c2ee0558409573dbdb3d685f6ffbe
admin <66056072@kmitl.ac.th>
2023-10-14 22:51:41 +0700

    test_ex19

--- src/ex19.py
+++ src/ex19.py
@@ -1,7 +1,9 @@
 """
 Exercise 19 : Password Generator
 """
+
 import random
+
 
 LOWER_LETTERS = 'abcdefghijklmnopqrstuvwxyz'    # 26
 UPPER_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'    # 26
@@ -11,6 +13,18 @@ SPECIAL = '~!@#$%^&*()_+'                       # 13
 ALL_CHARS = LOWER_LETTERS + UPPER_LETTERS + NUMBERS + SPECIAL
 
 
-def gen_password(param):
-    # TODO : complete this
-    pass
+def gen_password(x):
+    # Fix : complete this
+    if x < 12:
+         x=12
+    pw = []
+    pw.append(LOWER_LETTERS[random.randint(0,25)])
+    pw.append(UPPER_LETTERS[random.randint(0,25)])
+    pw.append(NUMBERS[random.randint(0,10)])
+    pw.append(SPECIAL[random.randint(0,13)])
+    print(pw)
+    while len(pw) < x:
+        pw.append(ALL_CHARS[random.randint(0,74)])
+        random.shuffle(pw)
+    return ''.join(pw)
+#%%
