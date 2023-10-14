d803110a6fb0131a0b8aa621f45f69abecda1212
admin <66056072@kmitl.ac.th>
2023-10-15 01:25:47 +0700

    test_ex06

--- src/ex06.py
+++ src/ex06.py
@@ -13,5 +13,18 @@ def ordinal_suffix(param):
     Returns:
         str: The ordinal suffix corresponding to the given number.
     """
-    # TODO : complete this
-    pass
+    # Fix : complete this
+    if param == 0:
+        return "0th"
+    if param == 1:
+        return "1st"
+    if param == 2:
+        return "2nd"
+    if param == 3:
+        return "3rd"
+    if param == 4:
+        return "4th"
+    if param >=10 and param<=100:
+        return str(param)+"th"
+    if param >=100:
+        return str(param)+"st"
