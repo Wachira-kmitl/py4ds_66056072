53b1011929de5635d033e04bc69fbc5a13b54659
admin <66056072@kmitl.ac.th>
2023-10-15 00:47:28 +0700

    test_ex24

--- src/ex24.py
+++ src/ex24.py
@@ -16,5 +16,8 @@ def get_time_every_15_min():
     Returns:
         str: The generated time string.
     """
-    # TODO : complete this
-    pass
+    # Fix : complete this
+    for meridiems in ['am','pm']:
+        for hours in ['12', '1', '2', '3', '4', '5', '6', '7', '8', '9','10', '11']:
+            for minutes in ['00', '15', '30', '45']:
+                print(hours+':'+minutes+' '+meridiems)
